86115652257b836618713698890c09694ec4b4b4
Iegor Stetsiura <biropka@gmail.com>
2016-06-27 16:33:31 +0300

    final

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,27 +1,52 @@
 $(document).ready(function () {
 
+    $('.see-more span').click(function () {
+        $(this).parent().find('span').toggleClass('active');
+        $(this).parent().toggleClass('opened');
+        if($(this).hasClass('to-open')){
+            $(this).parents('.programs').find('.openable').slideDown(600);
+         } else {
+            $(this).parents('.programs').find('.openable').slideUp(0);
+         }
+    });
+
     /** sliders **/
 
     $('.slider').slick({
 
         appendArrows: '.slider-control',
         prevArrow: '.left-control',
-        nextArrow: '.right-control'
+        nextArrow: '.right-control',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
 
     });
 
     $('.reviews-slider').slick({
 
-        arrows: false
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 6000
 
     });
 
     /** end of sliders **/
 
-    $('.see-more span').click(function () {
-        $(this).parent().find('span').toggleClass('active');
-        $(this).parents('.programs').find('.openable').slideToggle();
-        $(this).parent().toggleClass('opened');
+    if ($(window).width() < '1024'){
+        $('.parallax-block').detach();
+    }
+
+    $(window).resize(function(){
+        if ($(window).width() < '1024'){
+            $('.parallax-block').detach();
+        }
     });
 
 });
---
+++ mail-1.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['inline-name'])) {$name = $_POST['inline-name'];}
+    if (isset($_POST['inline-phone'])) {$phone = $_POST['inline-phone'];}
+    if (isset($_POST['inline-email'])) {$email = $_POST['inline-email'];}
+    if (isset($_POST['inline-spec'])) {$spec = $_POST['inline-spec'];}
+    if (isset($_POST['formData'])) {$formData = $_POST['formData'];}
+
+    $to = "biropka@gmail.com"; /*Укажите адрес, на который должно приходить письмо*/
+    $sendfrom   = "Поступивуз.рф"; /*Укажите адрес, с которого будет приходить письмо, можно не настоящий, нужно для формирования заголовка письма*/
+    $headers  = "From: " . strip_tags($sendfrom) . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($sendfrom) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+    $subject = "$formData";
+    $message = "$formData <br><b>Заявка на поступление </b> <br> <b>Имя клиента:</b> $name <br><b>Телефон:</b> $phone <br><b>Email:</b> $email <br><b>Специальность:</b> $spec";
+    $send = mail ($to, $subject, $message, $headers);
+    if ($send == 'true')
+    {
+    echo '<p class="success">Спасибо за обращение к нам!</p>';
+    }
+    else
+    {
+    echo '<p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p>';
+    }
+} else {
+    http_response_code(403);
+    echo "Попробуйте еще раз";
+}
+?>
