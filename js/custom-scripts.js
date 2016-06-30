5238245a3150e5043cba3b9cdc868c5a2de8a459
Iegor Stetsiura <biropka@gmail.com>
2016-07-01 00:25:33 +0300

    final

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -14,32 +14,65 @@ $(document).ready(function () {
         $('#inline-form').validatr({
             showall: true,
             valid: submitInlineForm
+        });
     });
-});
-function submitInlineForm() {
-    // Получение ID формы
-    var formID = $(this).attr('id');
-    // Добавление решётки к имени ID
-    var formNm = $('#' + formID);
-    var scriptFile;
-    if (formID == 'inline-form') scriptFile = 'mail-1.php';
-    if (formID == 'callback-form') scriptFile = 'mail-2.php';
-    $.ajax({
-        type: "POST",
-        url: scriptFile,
-        data: formNm.serialize(),
-        success: function (data) {
-            // Вывод текста результата отправки
-            if (formID == 'inline-form') {
-                $('.inline-success-block').fadeIn();
-            } else if (formID == 'callback-form') {
-                $('.callback-success-block').fadeIn();
-            }
-        },
-        error: function (jqXHR, text, error) {}
+
+    function submitInlineForm() {
+        // Получение ID формы
+        var formID = $(this).attr('id');
+        // Добавление решётки к имени ID
+        var formNm = $('#' + formID);
+        var scriptFile;
+        if (formID == 'inline-form') scriptFile = 'mail-1.php';
+        if (formID == 'callback-form') scriptFile = 'mail-2.php';
+        $.ajax({
+            type: "POST",
+            url: scriptFile,
+            data: formNm.serialize(),
+            success: function (data) {
+                // Вывод текста результата отправки
+                if (formID == 'inline-form') {
+                    $('.inline-success-block').fadeIn();
+                } else if (formID == 'callback-form') {
+                    $('.callback-success-block').fadeIn();
+                }
+            },
+            error: function (jqXHR, text, error) {}
+        });
+        return false;
+    }
+
+    $(function($) {
+        $('#callback-form').validatr({
+            showall: true,
+            valid: submitCallbackForm
+        });
     });
-    return false;
-}
+
+    function submitCallbackForm() {
+        // Получение ID формы
+        var formID = $(this).attr('id');
+        // Добавление решётки к имени ID
+        var formNm = $('#' + formID);
+        var scriptFile;
+        if (formID == 'inline-form') scriptFile = 'mail-1.php';
+        if (formID == 'callback-form') scriptFile = 'mail-2.php';
+        $.ajax({
+            type: "POST",
+            url: scriptFile,
+            data: formNm.serialize(),
+            success: function (data) {
+                // Вывод текста результата отправки
+                if (formID == 'inline-form') {
+                    $('.inline-success-block').fadeIn();
+                } else if (formID == 'callback-form') {
+                    $('.callback-success-block').fadeIn();
+                }
+            },
+            error: function (jqXHR, text, error) {}
+        });
+        return false;
+    }
 
     $(function($){
         $('.user-phone').mask("+7(999) 999-99-99");
