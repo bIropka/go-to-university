93e70a7ea09a44d57034355cbbe06b1a1b12746f
Iegor Stetsiura <biropka@gmail.com>
2016-07-01 00:12:01 +0300

    final

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -10,6 +10,41 @@ $(document).ready(function () {
          }
     });
 
+    $(function($) {
+        $('#inline-form').validatr({
+            showall: true,
+            valid: submitInlineForm
+    });
+});
+function submitInlineForm() {
+    // Получение ID формы
+    var formID = $(this).attr('id');
+    // Добавление решётки к имени ID
+    var formNm = $('#' + formID);
+    var scriptFile;
+    if (formID == 'inline-form') scriptFile = 'mail-1.php';
+    if (formID == 'callback-form') scriptFile = 'mail-2.php';
+    $.ajax({
+        type: "POST",
+        url: scriptFile,
+        data: formNm.serialize(),
+        success: function (data) {
+            // Вывод текста результата отправки
+            if (formID == 'inline-form') {
+                $('.inline-success-block').fadeIn();
+            } else if (formID == 'callback-form') {
+                $('.callback-success-block').fadeIn();
+            }
+        },
+        error: function (jqXHR, text, error) {}
+    });
+    return false;
+}
+
+    $(function($){
+        $('.user-phone').mask("+7(999) 999-99-99");
+    });
+
     /** sliders **/
 
     $('.slider').slick({
